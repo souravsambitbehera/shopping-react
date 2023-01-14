1b6aeff41a29d5717ba5ca7182376c5cc5c1ecad
sourav sambit behera <souravsambit12@gmail.com>
2023-01-14 13:41:09 +0530

    commit(nothing)

--- src/components/event-component/event.component.js
+++ src/components/event-component/event.component.js
@@ -3,25 +3,63 @@ export function EventComponent () {
     const[products] = useState([{id:1,name:"sourav"},{id:2,name:"sambit"}]);
 
     function onClickButton (event) {
+
          let result = products.find(product =>
             product.id==event.target.id);
-            document.write(result.name + "<br>" + result.id)
+            
     }
+
 
     return (
         <div className="container-fluid">
-            <h2>products</h2>
-            <div>{
-                products.map(product =>
-                    <div>
-                        <p>{product.name}</p>
-                        <button id={product.id} onClick={onClickButton}  ><span className="bi bi-cart4"></span>Add to cart</button>
-                    </div> )
-            }
-            </div>
+
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        products.map(product =>
+                            <tr>
+                                <td>{product.id}</td>
+                                <td>{product.name}</td>
+                                <td>
+                                    <button  className="btn btn-danger"><span id={product.id} className="bi bi-trash" onClick={onClickButton}></span>
+                                
+                                    </button>
+                                    <button className="btn btn-warning ms-2"><span className="bi bi-pen "></span></button>
+
+                                </td>
+                            </tr> )
+                    }
+                </tbody>
+
+            </table>
+
         </div>
     )
-}
+
+    }
+
+
+
+//     return (
+//         <div className="container-fluid">
+//             <h2>products</h2>
+//             <div>{
+//                 products.map(product =>
+//                     <div>
+//                         <p>{product.name}</p>
+//                         <button id={product.id} onClick={onClickButton}  ><span className="bi bi-cart4"></span>Add to cart</button>
+//                     </div> )
+//             }
+//             </div>
+//         </div>
+//     )
+// }
 
 
 
