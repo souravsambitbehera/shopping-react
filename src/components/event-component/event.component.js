474cf4e51fcbc4d13eb53cc8348cd5f12d5991cc
sourav sambit behera <souravsambit12@gmail.com>
2023-01-19 22:52:30 +0530

    event practice

--- src/components/event-component/event.component.js
+++ src/components/event-component/event.component.js
@@ -1,51 +1,95 @@
 import { useState } from "react";
 export function EventComponent () {
-    const[products,setProducts] = useState([{id:1,name:"sourav"},{id:2,name:"sambit"}]);
-
-    function OnClickButton (event) {
-
-        setProducts(products.filter(product =>
-            product.id !=event.target.id));
-            // result.splice((result.id-1),result.id)
-            // let delete_item = result.splice(result.id);
-            // document.write(products.slice(result.id))
-            
+    const[phone,setPhone] = useState('phone_img1.jpg')
+    function openImage (e) {
+        switch(e.target.id) {
+            case "1":
+            setPhone('phone_img1.jpg');
+            break;
+            case "2":
+            setPhone('phone_img2.jpg');
+            break;
+            case "3":
+            setPhone('phone_img3.jpg');
+            break;
+        }
     }
-
-
     return (
-        <div className="container-fluid">
-
-            <table className="table table-hover">
-                <thead>
-                    <tr>
-                        <th>Id</th>
-                        <th>Name</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        products.map(product =>
-                            <tr key={product.id}>
-                                <td>{product.id}</td>
-                                <td>{product.name}</td>
-                                <td>
-                                    <button id={product.id} onClick={OnClickButton}  className="btn btn-danger"><span className="bi bi-trash"></span>
-                                    delete
-                                    </button>
-                                    <button className="btn btn-warning ms-2"><span className="bi bi-pen "></span></button>
-
-                                </td>
-                            </tr> )
-                    }
-                </tbody>
-
-            </table>
-
+        <div>
+            <div className="container-fluid">
+           <h2>Mouse over to phone</h2>
+           <img src={phone}  width="900" height="900"/>
+           <p className="mt-3">
+             <div>
+                <span id="1" onMouseOver={openImage} style={{backgroundColor:'red', color:'white', cursor: 'grab'}} className="me-2 p-2">
+                    <img src="" alt="" srcset="" />
+                </span>
+                <span id="2" onMouseOver={openImage} style={{backgroundColor:'green', color:'white', cursor: 'grab'}} className="me-2 p-2">Green</span>
+                <span id="3" onMouseOver={openImage} style={{backgroundColor:'blue', color:'white', cursor: 'grab'}} className="me-2 p-2">Blue</span>
+             </div>
+           </p>
+        </div>
         </div>
     )
+}
 
-    }
+
+
+
+
+
+
+
+
+
+
+// {
+//     const[products,setProducts] = useState([{id:1,name:"sourav"},{id:2,name:"sambit"}]);
+
+//     function OnClickButton (event) {
+
+//         setProducts(products.filter(product =>
+//             product.id !=event.target.id));
+//             // result.splice((result.id-1),result.id)
+//             // let delete_item = result.splice(result.id);
+//             // document.write(products.slice(result.id))
+            
+//     }
+
+
+//     return (
+//         <div className="container-fluid">
+
+//             <table className="table table-hover">
+//                 <thead>
+//                     <tr>
+//                         <th>Id</th>
+//                         <th>Name</th>
+//                     </tr>
+//                 </thead>
+//                 <tbody>
+//                     {
+//                         products.map(product =>
+//                             <tr key={product.id}>
+//                                 <td>{product.id}</td>
+//                                 <td>{product.name}</td>
+//                                 <td>
+//                                     <button id={product.id} onClick={OnClickButton}  className="btn btn-danger"><span className="bi bi-trash"></span>
+//                                     delete
+//                                     </button>
+//                                     <button className="btn btn-warning ms-2"><span className="bi bi-pen "></span></button>
+
+//                                 </td>
+//                             </tr> )
+//                     }
+//                 </tbody>
+
+//             </table>
+
+//         </div>
+//     )
+
+//     }
 
 
 
